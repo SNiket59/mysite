7a20982a2f6587c6cfcab936f384536d6fe176ff
SNiket59 <nsheth2007@gmail.com>
2023-10-29 12:02:46 -0500

    error fixed

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 app=Flask(__name__, static_url_path='/static')
-@app.route('/', methods['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
